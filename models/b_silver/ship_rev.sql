72290bb854d09072742be04af677ac6a7c4f3faf
Miles Freeborn <miles.freeborn@dbtlabs.com>
2024-10-16 18:22:30 +0000

    updated model

--- models/b_silver/ship_rev.sql
+++
@@ -1,3 +0,0 @@
-select shipmode, revenue as ship_rev
-from {{ ref('fct_order_lineitems') }}
-group by shipmode
--- models/c_gold/agg_order_revenue_assert.sql
+++ models/c_gold/agg_order_revenue_assert.sql
@@ -2,7 +2,6 @@
 
 with
     fact_data as (
-
         select
             orderkey,
             custkey,
@@ -35,11 +34,9 @@ with
             region_name,
             revenue
         from {{ ref("fct_order_customer") }}
-
     ),
 
     revenue_data as (
-
         select
             date_part('year', orderdate) as year,
             returnflag,
@@ -48,11 +45,9 @@ with
             nation_name,
             revenue
         from fact_data
-
     ),
 
     aggregated_revenue as (
-
         select
             year,
             returnflag,
@@ -62,8 +57,17 @@ with
             sum(revenue) as total_revenue
         from revenue_data
         group by year, returnflag, shipmode, region_name, nation_name
+    ),
 
+    final_data as (
+        select
+            *,
+            {{ dbt_assertions.assertions(column='exceptions') }} -- Apply the assertion to the 'exceptions' column
+        from aggregated_revenue
     )
 
+-- Filter out rows that fail the 'YEAR < 1998' check based on 'exceptions'
 select *
-from aggregated_revenue
+from final_data
+where {{ dbt_assertions.assertions_filter(column='exceptions') }}
+order by year desc
