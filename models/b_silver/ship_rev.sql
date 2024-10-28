2f4d5d47e998e39405693e69f74ba3f2c7de6cb9
Miles Freeborn <miles.freeborn@dbtlabs.com>
2024-10-28 20:49:00 +0000

    added shipping rev

--- models/b_silver/ship_rev.sql
+++ models/b_silver/ship_rev.sql
@@ -1,3 +1,5 @@
-select shipmode, sum(revenue) as shipping_rev
-from {{ ref('fct_order_customer') }}
+select
+    shipmode,
+    sum(revenue) as ship_rev
+from {{ ref('fct_order_lineitems') }}
 group by shipmode
