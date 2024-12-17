5acf1cd1828f1fcde70d8e4ea81e05072c219104
Miles Freeborn <miles.freeborn@dbtlabs.com>
2024-12-17 20:47:45 +0000

    added shipping revenue

---
+++ models/b_silver/ship_rev.sql
@@ -0,0 +1,9 @@
+SELECT 
+    shipmode, 
+    YEAR(shipdate) AS ship_year,
+    ROUND(SUM(revenue)) AS total_revenue
+FROM 
+    {{ ref('fct_order_customer') }}
+GROUP BY 
+    shipmode,
+    YEAR(shipdate)
