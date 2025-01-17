3185e3fa9e10c69724cb5641084028b1609de1a3
Miles Freeborn <miles.freeborn@dbtlabs.com>
2025-01-17 19:46:45 +0000

    added a new model

--- models/b_silver/shipping_rev.sql
+++ models/b_silver/shipping_rev.sql
@@ -1,9 +1,9 @@
 SELECT 
     shipmode, 
-    DATE_TRUNC('year', shipdate) AS ship_year,
+    YEAR(shipdate) AS ship_year,
     ROUND(SUM(revenue)) AS total_revenue
 FROM 
     {{ ref('fct_order_customer') }}
 GROUP BY 
-    shipmode,
-    DATE_TRUNC('year', shipdate)
+    shipmode, 
+    YEAR(shipdate)
